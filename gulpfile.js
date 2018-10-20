c855aec9deb7b4cc42b2eb53c60781b4355881aa
radiium <remip.dev@gmail.com>
2018-10-20 14:38:59 +0200

    Clean and add sass tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@
 var gulp = require('gulp')
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+sass.compiler = require('node-sass');
 
+// Compress JS files
 gulp.task('compress', function() {
     return gulp.src([
-        'assets/js/**/*.js',
-        '!assets/js/**/*.min.js'
+        'assets/scripts/**/*.js',
+        '!assets/scripts/**/*.min.js'
     ])
     .pipe(uglify().on('error', function(e){
         console.log(e);
@@ -14,5 +17,18 @@ gulp.task('compress', function() {
     .pipe(rename(function(path) {
         path.extname = ".min.js";
     }))
-    .pipe(gulp.dest('assets/js'));
+    .pipe(gulp.dest('assets/scripts'));
+});
+
+// Compil SASS files
+gulp.task('sass', function () {
+    return gulp.src('./assets/styles/source.scss')
+        .pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError))
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('./'));
+});
+
+// Watch SASS files change
+gulp.task('sass:watch', function () {
+    gulp.watch('./assets/styles/**/*.scss', ['sass']);
 });
