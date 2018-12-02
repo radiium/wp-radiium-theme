54e7a16f34bc516fb8b3d409724568cedb06e593
radiium <remip.dev@gmail.com>
2018-12-02 17:45:23 +0100

    Add footer and fix nav mobil height

--- footer.php
+++ footer.php
@@ -1,3 +1,32 @@
+
+        <!-- Footer -->
+        <div class="footerWrapper">
+
+            <div class="navFooter">
+                <!-- Social icons -->
+                <div class="socialBtnWrapper">
+                    <a class="socialBtn" target="_blank" href="https://www.instagram.com/_unlink/" title="Link to instagram!">
+                    <img src="<?php echo get_site_url() ?>/wp-content/themes/radiium/assets/images/instache-logo.svg" alt="">
+                    </a>
+
+                    <a class="socialBtn" target="_blank" href="https://www.facebook.com/Rémi-Rémi-135772170533399/" title="Link to facebook!">
+                    <img src="<?php echo get_site_url() ?>/wp-content/themes/radiium/assets/images/fache-logo.svg" alt="">
+                    </a>
+
+                    <a class="socialBtn gith" target="_blank" href="https://www.github.com/radiium/" title="Link to github!">
+                    <img src="<?php echo get_site_url() ?>/wp-content/themes/radiium/assets/images/gitgit-logo.svg" alt="">
+                    </a>
+                </div>
+
+                <!-- Copyright infos -->
+                <p class="copyright" >
+                    <span>&copy;&nbsp;</span>
+                    <span><?php bloginfo( 'name' ); ?>,&nbsp;</span>
+                    <span><?php echo date( 'Y' ); ?></span>
+                </p>
+            </div>
+        </div>
+
         <!-- WP footer's scripts -->
         <div class="footerScripts">
             <?php wp_footer(); ?>
