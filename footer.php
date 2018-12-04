3cfd7ec59cd94ebe0a2a6b8dc308df3608766567
radiium <remip.dev@gmail.com>
2018-12-04 01:09:02 +0100

    Remove mamoto

--- footer.php
+++ footer.php
@@ -30,22 +30,6 @@
         <!-- WP footer's scripts -->
         <div class="footerScripts">
             <?php wp_footer(); ?>
-
-            <!-- Matomo -->
-            <script type="text/javascript">
-                var _paq = _paq || [];
-                /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-                _paq.push(['trackPageView']);
-                _paq.push(['enableLinkTracking']);
-                (function() {
-                var u="//137.74.175.220/matomo/";
-                _paq.push(['setTrackerUrl', u+'piwik.php']);
-                _paq.push(['setSiteId', '1']);
-                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-                g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-                })();
-            </script>
-            <!-- End Matomo Code -->
         </div>
 
     </div><!-- END wrapper -->
